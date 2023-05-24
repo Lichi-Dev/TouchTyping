4e6a30e77bc6bfed31237322c2d9278bb0d12335
Bonny Boben <bonnybobencr7@gmail.com>
2023-05-24 16:01:38 +0530

    second commit

--- touch-typing/src/App.js
+++ touch-typing/src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { data } from "./Data/data";
+
+const getCloud = () =>
+  data.split(" ").sort(() => (Math.random() > 0.5 ? 1 : -1));
+
+function Timer(props) {
+  const { correctedWords, startCounting, totalWords } = props;
+  const [timeElapsed, setTimeElapsed] = useState(300);
+  useEffect(() => {
+    if (startCounting) {
+      setInterval(() => {
+        setTimeElapsed((oldTime) => oldTime - 1);
+      }, 1000);
+    }
+  }, [startCounting]);
+
+  const minutes = (300 - timeElapsed) / 60;
+  return (
+    <div>
+      <p>Time: {timeElapsed}secs</p>
+      <p>Speed: {(correctedWords / minutes || 0).toFixed(2)} WPM</p>
+      <p>Accuracy:{(correctedWords / totalWords) * 100 || 0}%</p>
+    </div>
+  );
+}
+
+function Word(props) {
+  const { text, active, correct } = props;
+  if (correct === true) {
+    return <span className="correct">{text} </span>;
+  }
+  if (correct === false) {
+    return <span className="incorrect">{text} </span>;
+  }
+  if (active) {
+    return <span className="active">{text} </span>;
+  }
+  return <span>{text} </span>;
+}
+
+Word = React.memo(Word);
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const cloud = useRef(getCloud());
+  const [activeWordIndex, setActiveWordIndex] = useState(0);
+  const [correctWordArray, setCorrectWordArray] = useState([]);
+  const [startCounting, setStartCounting] = useState(false);
+
+  function processInput(value) {
+    if (!startCounting) {
+      setStartCounting(true);
+    }
+
+    if (value.endsWith(" ")) {
+      // the user has finished this word
+      setActiveWordIndex((index) => index + 1);
+      setUserInput("");
+
+      setCorrectWordArray((data) => {
+        const word = value.trim();
+        const newResult = [...data];
+        newResult[activeWordIndex] = word === cloud.current[activeWordIndex];
+        return newResult;
+      });
+    } else {
+      setUserInput(value);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Typing Test</h1>
+      <Timer
+        startCounting={startCounting}
+        correctedWords={correctWordArray.filter(Boolean).length}
+        totalWords={cloud.current.length}
+      />
+      <p>
+        {cloud.current.map((word, index) => {
+          return (
+            <Word
+              text={word}
+              active={index === activeWordIndex}
+              correct={correctWordArray[index]}
+            />
+          );
+        })}
+      </p>
+      <input
+        type="text"
+        value={userInput}
+        onChange={(e) => processInput(e.target.value)}
+      />
     </div>
   );
 }
---
+++ touch-typing/src/Data/data.js
@@ -0,0 +1,2 @@
+export const data = `a s d f j k l ; aa as ad af aj ak al a; sa s sd sf sj sk sl s; da ds d df dj dk dl dd; fa fs fd ff fj fk fl f; ja js jd jf jj jk jl j; ka ks kd kf kj kk kl k; la ls ld lf lj lk ll l; ;a ;s ;d ;f ;j ;k ;l ;; aaaaaf aaaaaj aaaaak aaaaal asda aslk asa dsl; kjl;s dsf; da;
+`;
